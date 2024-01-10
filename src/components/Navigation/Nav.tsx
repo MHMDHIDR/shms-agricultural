86114b2b4b0ab51f49b5a848ef4a0994b45bc10f
Mohammed Haydar <mr.hamood277@gmail.com>
2024-01-10 13:54:20 +0000

    Initial Commit

---
+++ src/components/Navigation/Nav.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import { ShoppingBag, Sun } from 'lucide-react'
+import { Button } from '../ui/button'
+import { ModeToggle } from './ModeToggle'
+
+export default function Nav() {
+  return (
+    <header className='bg-background text-foreground p-6'>
+      <nav className='max-w-6xl py-6 m-auto'>
+        <ul className='flex justify-between items-center'>
+          <li>
+            <Link className='font-bold' href={'/'}>
+              <Sun className='text-[#FDB813]' />
+            </Link>
+          </li>
+          <div className='flex items-center gap-8'>
+            <li>
+              <ModeToggle />
+            </li>
+            <li>
+              <Button size={'icon'} variant='outline'>
+                <ShoppingBag strokeWidth={'1.5px'} className='text-foreground text-lg ' />
+              </Button>
+            </li>
+            <li>بروفايل المستخدم</li>
+          </div>
+        </ul>
+      </nav>
+    </header>
+  )
+}
