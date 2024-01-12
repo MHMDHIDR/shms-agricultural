52373dc1d576d16906de6f03afc471563163951e
Mohammed Haydar <mr.hamood277@gmail.com>
2024-01-12 17:05:29 +0000

    Using Clerk for auth

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,3 @@
-import { Session } from 'next-auth'
-
 export type UserProps = {
   shms_id?: string
   shms_fullname?: string
@@ -16,40 +14,4 @@ export type UserProps = {
   userAccountStatus?: 'active' | 'block'
   userResetPasswordToken?: string
   userResetPasswordExpires?: string
-} & Session['user']
-
-export type NextUserProviderProps = {
-  userFullName?: string
-  shms_email?: string
-  userAccountStatus?: string
-  userAccountType?: string
-  signupMethod?: string
-  picture?: string
-}
-
-export type LoggedInUserProps =
-  | (Session & {
-      token?: {
-        user: UserProps
-        email: NextUserProviderProps['shms_email']
-        picture: NextUserProviderProps['picture']
-      }
-      session?: {
-        user: UserProps
-      }
-    })
-  | null
-
-export type ModalProps = {
-  msg?: string
-  extraComponents?: React.ReactNode
-  status?: React.ReactNode
-  modalHidden?: string
-  classes?: string
-  redirectLink?: string
-  redirectTime?: number
-  btnName?: string
-  btnLink?: string
-  ctaConfirmBtns?: string[]
-  ctaSpecialBtns?: string[]
 }
