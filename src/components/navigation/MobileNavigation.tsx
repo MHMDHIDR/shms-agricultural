addea1aebcd6b379a2dc766bf842b7ca08e76fd8
Mohammed Haydar <mr.hamood277@gmail.com>
2024-01-26 12:16:24 +0000

    Update MobileNavigation component with new menu items

--- src/components/navigation/MobileNavigation.tsx
+++ src/components/navigation/MobileNavigation.tsx
@@ -32,20 +32,36 @@ export default function MobileNavigation({
       collapsible
       className={cn(`w-full transition rtl ${isOpen ? 'block' : 'hidden'}`, className)}
     >
+      {/* الخدمات */}
+      <AccordionItem value='item-3'>
+        <AccordionTrigger>الخدمات</AccordionTrigger>
+        <AccordionContent>
+          <ul className='grid min-w-screen w-dvw gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] right-0'>
+            {MenuItems.map(component => (
+              <NavigationListItem
+                key={component.title}
+                title={component.title}
+                href={component.href}
+              >
+                {component.description}
+              </NavigationListItem>
+            ))}
+          </ul>
+        </AccordionContent>
+      </AccordionItem>
+      {/* المشاريع الاستثمارية */}
+      <AccordionItem value='item-2'>
+        <AccordionTrigger isDropDown={false}>
+          <Link href='/projects' onClick={() => setIsOpen(open => !open)} passHref>
+            المشاريع الاستثمارية
+          </Link>
+        </AccordionTrigger>
+      </AccordionItem>
+      {/* عن شمس */}
       <AccordionItem value='item-1'>
         <AccordionTrigger>عن شمس</AccordionTrigger>
         <AccordionContent>
           <ul className='grid gap-3 p-4 min-w-screen w-dvw md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
-            <NavigationListItem
-              href='/about'
-              title='عن شمس'
-              onClick={() => setIsOpen(open => !open)}
-            ></NavigationListItem>
-            <NavigationListItem
-              href='/contact'
-              title='تواصل معنا'
-              onClick={() => setIsOpen(open => !open)}
-            ></NavigationListItem>
             <NavigationMenuLink asChild>
               <div className={isAuth ? `flex items-center justify-between` : ``}>
                 <NavigationListItem
@@ -74,29 +90,16 @@ export default function MobileNavigation({
                 )}
               </div>
             </NavigationMenuLink>
-          </ul>
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value='item-2'>
-        <AccordionTrigger isDropDown={false}>
-          <Link href='/projects' onClick={() => setIsOpen(open => !open)} passHref>
-            المشاريع الاستثمارية
-          </Link>
-        </AccordionTrigger>
-      </AccordionItem>
-      <AccordionItem value='item-3'>
-        <AccordionTrigger>الخدمات</AccordionTrigger>
-        <AccordionContent>
-          <ul className='grid min-w-screen w-dvw gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] right-0'>
-            {MenuItems.map(component => (
-              <NavigationListItem
-                key={component.title}
-                title={component.title}
-                href={component.href}
-              >
-                {component.description}
-              </NavigationListItem>
-            ))}
+            <NavigationListItem
+              href='/contact'
+              title='تواصل معنا'
+              onClick={() => setIsOpen(open => !open)}
+            ></NavigationListItem>
+            <NavigationListItem
+              href='/about'
+              title='من نحن'
+              onClick={() => setIsOpen(open => !open)}
+            ></NavigationListItem>
           </ul>
         </AccordionContent>
       </AccordionItem>
