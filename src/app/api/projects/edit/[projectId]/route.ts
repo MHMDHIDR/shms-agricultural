ed8e198bff0ad52414eac4b9b6e635d370bbef60
Mohammed Haydar <mr.hamood277@gmail.com>
2024-02-23 22:46:56 +0000

    Refactor project update query

--- src/app/api/projects/edit/[projectId]/route.ts
+++ src/app/api/projects/edit/[projectId]/route.ts
@@ -59,82 +59,77 @@ export async function PATCH(
     }
 
     // Step 1: Retrieve the old value of shms_project_total_stocks before updating it
-    const oldTotalStocks = project.shms_project_total_stocks
+    // const oldTotalStocks = project.shms_project_total_stocks
+
+    let updateQuery = ''
+    let updateValues = []
+    if (updateImg) {
+      updateQuery = `UPDATE projects SET shms_project_images = ? WHERE shms_project_id = ?`
+      updateValues = [JSON.stringify(shms_project_images), projectId]
+    } else if (updatePercentage) {
+      updateQuery = `UPDATE projects SET shms_project_special_percentage = ?, shms_project_special_percentage_code = ? WHERE shms_project_id = ?`
+      updateValues = [
+        shms_project_special_percentage,
+        shms_project_special_percentage_code,
+        projectId
+      ]
+    } else {
+      updateQuery = `UPDATE projects SET
+          shms_project_images = ?,
+          shms_project_name = ?,
+          shms_project_location = ?,
+          shms_project_start_date = ?,
+          shms_project_end_date = ?,
+          shms_project_invest_date = ?,
+          shms_project_profits_collect_date = ?,
+          shms_project_total_stocks = ?,
+          shms_project_stock_price = ?,
+          shms_project_stock_profits = ?,
+          shms_project_special_percentage = ?,
+          shms_project_special_percentage_code = ?,
+          shms_project_description = ?,
+          shms_project_terms = ?,
+          shms_project_study_case = ?,
+          shms_project_study_case_visibility = ?,
+          shms_project_status = ?
+        WHERE shms_project_id = ?`
+      updateValues = [
+        JSON.stringify(shms_project_images),
+        shms_project_name,
+        shms_project_location,
+        shms_project_start_date,
+        shms_project_end_date,
+        shms_project_invest_date,
+        shms_project_profits_collect_date,
+        shms_project_total_stocks,
+        shms_project_stock_price,
+        shms_project_stock_profits,
+        shms_project_special_percentage || null,
+        shms_project_special_percentage_code || null,
+        shms_project_description,
+        shms_project_terms,
+        JSON.stringify(shms_project_study_case),
+        shms_project_study_case_visibility,
+        shms_project_status,
+        projectId
+      ]
+    }
 
     // Update project
-    const updateProject = updateImg
-      ? ((await connectDB(
-          `UPDATE projects SET
-        shms_project_images = ?
-      WHERE shms_project_id = ?`,
-          [JSON.stringify(shms_project_images), projectId]
-        )) as ResultSetHeader)
-      : updatePercentage
-      ? ((await connectDB(
-          `UPDATE projects SET
-            shms_project_special_percentage = ?,
-            shms_project_special_percentage_code = ?
-          WHERE shms_project_id = ?`,
-          [
-            shms_project_special_percentage,
-            shms_project_special_percentage_code,
-            projectId
-          ]
-        )) as ResultSetHeader)
-      : ((await connectDB(
-          `UPDATE projects SET
-            shms_project_images = ?,
-            shms_project_name = ?,
-            shms_project_location = ?,
-            shms_project_start_date = ?,
-            shms_project_end_date = ?,
-            shms_project_invest_date = ?,
-            shms_project_profits_collect_date = ?,
-            shms_project_total_stocks = ?,
-            shms_project_stock_price = ?,
-            shms_project_stock_profits = ?,
-            shms_project_special_percentage = ?,
-            shms_project_special_percentage_code = ?,
-            shms_project_description = ?,
-            shms_project_terms = ?,
-            shms_project_study_case = ?,
-            shms_project_study_case_visibility = ?,
-            shms_project_status = ?
-          WHERE shms_project_id = ?`,
-          [
-            JSON.stringify(shms_project_images),
-            shms_project_name,
-            shms_project_location,
-            shms_project_start_date,
-            shms_project_end_date,
-            shms_project_invest_date,
-            shms_project_profits_collect_date,
-            shms_project_total_stocks,
-            shms_project_stock_price,
-            shms_project_stock_profits,
-            shms_project_special_percentage || null,
-            shms_project_special_percentage_code || null,
-            shms_project_description,
-            shms_project_terms,
-            JSON.stringify(shms_project_study_case),
-            shms_project_study_case_visibility,
-            shms_project_status,
-            projectId
-          ]
-        )) as ResultSetHeader)
+    const updateProject = (await connectDB(updateQuery, updateValues)) as ResultSetHeader
     const { affectedRows: projectUpdated } = updateProject as ResultSetHeader
 
-    // Step 2: Calculate the difference between the old and new values
-    const stocksDifference = oldTotalStocks - shms_project_total_stocks
+    // // Step 2: Calculate the difference between the old and new values
+    // const stocksDifference = oldTotalStocks - shms_project_total_stocks
 
-    // Step 3: Subtract this difference from the current value of shms_project_available_stocks
-    const newAvailableStocks = project.shms_project_available_stocks - stocksDifference
+    // // Step 3: Subtract this difference from the current value of shms_project_available_stocks
+    // const newAvailableStocks = project.shms_project_available_stocks - stocksDifference
 
-    // Update shms_project_available_stocks with the new calculated value
-    await connectDB(
-      `UPDATE projects SET shms_project_available_stocks = ? WHERE shms_project_id = ?`,
-      [newAvailableStocks, projectId]
-    )
+    // // Update shms_project_available_stocks with the new calculated value
+    // await connectDB(
+    //   `UPDATE projects SET shms_project_available_stocks = ? WHERE shms_project_id = ?`,
+    //   [newAvailableStocks, projectId]
+    // )
 
     return projectUpdated
       ? new Response(
