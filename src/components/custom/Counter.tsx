5222a361d52fe265ce9ebeb5ecd25c68274adb4d
Mohammed Haydar <mr.hamood277@gmail.com>
2024-02-26 10:53:13 +0000

    Add intersection observer for counter animation

--- src/components/custom/Counter.tsx
+++ src/components/custom/Counter.tsx
@@ -1,26 +1,43 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 export default function Counter({ number }: { number: number }) {
   const [count, setCount] = useState(1)
+  const counterRef = useRef(null)
 
   useEffect(() => {
-    const animationDuration = 2000
-    const step = Math.ceil(number / (animationDuration / 100))
-    let currentCount = 1
+    const observer = new IntersectionObserver(
+      entries => {
+        if (entries[0]?.isIntersecting) {
+          const animationDuration = 2000
+          const step = Math.ceil(number / (animationDuration / 100))
+          let currentCount = 1
 
-    const interval = setInterval(() => {
-      currentCount += step
-      if (currentCount >= number) {
-        clearInterval(interval)
-        currentCount = number
+          const interval = setInterval(() => {
+            currentCount += step
+            if (currentCount >= number) {
+              clearInterval(interval)
+              currentCount = number
+            }
+            setCount(currentCount)
+          }, 100)
+
+          return () => clearInterval(interval)
+        }
+      },
+      // 0.5 تعني انه عندما يكون العنصر مرئي بنسبة 50% فقط
+      { threshold: 0.5 }
+    )
+
+    observer.observe(counterRef?.current!)
+
+    return () => {
+      if (counterRef.current) {
+        observer.unobserve(counterRef.current)
       }
-      setCount(currentCount)
-    }, 100)
-
-    return () => clearInterval(interval)
+    }
   }, [number])
 
-  return <>{count}</>
+  return <span ref={counterRef}>{count}</span>
 }
