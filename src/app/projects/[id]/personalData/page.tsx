f03143f626b2cdecb3e76fd8b313c3449a87064d
Mohammed Haydar <mr.hamood277@gmail.com>
2024-02-27 01:02:17 +0000

    Remove unnecessary code in PersonalData page

--- src/app/projects/[id]/personalData/page.tsx
+++ src/app/projects/[id]/personalData/page.tsx
@@ -135,8 +135,6 @@ export default function PersonalData({
 
   return !session ? (
     replace('/')
-  ) : !session?.expires ? (
-    <LoadingPage />
   ) : (
     <Layout>
       {stocksPurchesed === 1 && (
