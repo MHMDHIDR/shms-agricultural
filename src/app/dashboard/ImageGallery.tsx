61b258a101605bc89771fd129d7849013cce5e48
Mohammed Haydar <mr.hamood277@gmail.com>
2024-02-28 09:38:14 +0000

    Add image gallery component and update styling

---
+++ src/app/dashboard/ImageGallery.tsx
@@ -0,0 +1,28 @@
+import { APP_TITLE } from '@/data/constants'
+import Image from 'next/image'
+
+export default function ImageGallery() {
+  return (
+    <div className='parent-container'>
+      <div className='child-container' id='one'>
+        <Image src='/ImageGallery/1.jpg' width={300} height={600} alt={APP_TITLE} />
+        <Image src='/ImageGallery/2.jpg' width={300} height={600} alt={APP_TITLE} />
+        <Image src='/ImageGallery/3.jpg' width={300} height={600} alt={APP_TITLE} />
+        <img src='/ImageGallery/4.jpg' width={300} height={600} alt={APP_TITLE} />
+        <Image src='/ImageGallery/5.jpg' width={300} height={600} alt={APP_TITLE} />
+      </div>
+      <div className='child-container' id='two'>
+        <Image src='/ImageGallery/6.jpg' width={300} height={600} alt={APP_TITLE} />
+        <Image src='/ImageGallery/12.jpg' width={300} height={600} alt={APP_TITLE} />
+        <Image src='/ImageGallery/7.jpg' width={300} height={600} alt={APP_TITLE} />
+        <Image src='/ImageGallery/8.jpg' width={300} height={600} alt={APP_TITLE} />
+      </div>
+      <div className='child-container' id='three'>
+        <Image src='/ImageGallery/9.jpg' width={300} height={600} alt={APP_TITLE} />
+        <Image src='/ImageGallery/13.jpg' width={300} height={600} alt={APP_TITLE} />
+        <Image src='/ImageGallery/10.jpg' width={300} height={600} alt={APP_TITLE} />
+        <img src='/ImageGallery/11.jpg' width={300} height={600} alt={APP_TITLE} />
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { API_URL } from '@/data/constants'
 import { UserProps } from '@/types'
 import Counter from '@/components/custom/Counter'
 import Divider from '@/components/custom/Divider'
+import ImageGallery from './dashboard/ImageGallery'
 
 export default async function Home() {
   // get images from backend api (uploaded to dashboard by admin stored in AWS S3)
@@ -28,7 +29,6 @@ export default async function Home() {
       <Slider images={images} />
       <section className='flex flex-col items-center justify-between min-h-screen p-8 md:p-24'>
         <h1 className='mb-8 text-2xl md:text-4xl md:mb-12'>مرحباً بكم في شمــس</h1>
-
         <div className='flex flex-col items-center md:flex-row md:gap-8'>
           <div className='flex items-center flex-col md:flex-row'>
             <div className='flex items-center justify-center w-full bg-transparent md:w-500 md:h-500 hover:opacity-90 hover:-translate-y-1 transition'>
@@ -59,7 +59,6 @@ export default async function Home() {
             </div>
           </div>
         </div>
-
         <div
           style={{ margin: 30 }}
           dir='rtl'
@@ -189,97 +188,11 @@ export default async function Home() {
             </div>
           </Link>
         </div>
-
         <Divider className='my-10' />
-
         <h1 className='mb-8 mt-14 text-2xl md:text-4xl md:mb-12 underline-hover'>
           معرض الصور
         </h1>
-
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(2, 1fr)',
-            gap: '15px',
-            marginTop: 30
-          }}
-        >
-          <div>
-            <Image
-              src='https://modo3.com/thumbs/fit630x300/142458/1478984574/%D9%85%D8%B1%D8%A7%D8%AD%D9%84_%D8%A7%D9%84%D8%B2%D8%B1%D8%A7%D8%B9%D8%A9.jpg'
-              alt='Image 1'
-              width={500}
-              height={500}
-              className='object-cover w-full h-full'
-              style={{
-                aspectRatio: '1/1',
-                borderWidth: 2.5,
-                borderColor: 'darkgreen',
-                borderRadius: 20,
-                marginTop: 15,
-                width: '90%',
-                height: '90%'
-              }}
-            />
-          </div>
-
-          <div>
-            <Image
-              src='http://media.kenanaonline.com/photos/1238365/1238365363/large_1238365363.jpg?1394703600'
-              alt='Image 1'
-              width={500}
-              height={500}
-              className='object-cover w-full h-full'
-              style={{
-                aspectRatio: '1/1',
-                borderWidth: 2.5,
-                borderColor: 'darkgreen',
-                borderRadius: 20,
-                marginTop: 15,
-                width: '90%',
-                height: '90%'
-              }}
-            />
-          </div>
-
-          <div style={{ marginTop: 30 }}>
-            <Image
-              src='https://mqalla.com/wp-content/uploads/%D8%AD%D8%B5%D8%A7%D8%AF-%D8%A7%D9%84%D9%82%D9%85%D8%AD.jpg'
-              alt='Image 1'
-              width={500}
-              height={500}
-              className='object-cover w-full h-full'
-              style={{
-                aspectRatio: '1/1',
-                borderWidth: 2.5,
-                borderColor: 'darkgreen',
-                borderRadius: 20,
-                marginTop: 15,
-                width: '90%',
-                height: '90%'
-              }}
-            />
-          </div>
-
-          <div style={{ marginTop: 30 }}>
-            <Image
-              src='https://maan.gov.ae/wp-content/uploads/2021/02/Social-Investment-Fund-2048x1365.jpg'
-              alt='Image 1'
-              width={500}
-              height={500}
-              className='object-cover w-full h-full'
-              style={{
-                aspectRatio: '1/1',
-                borderWidth: 2.5,
-                borderColor: 'darkgreen',
-                borderRadius: 20,
-                marginTop: 15,
-                width: '90%',
-                height: '90%'
-              }}
-            />
-          </div>
-        </div>
+        <ImageGallery />
       </section>
     </Layout>
   )
