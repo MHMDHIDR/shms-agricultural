4a7fe86e2506355ca5c4372ad50d98c4e2876cd7
Mohammed Haydar <mr.hamood277@gmail.com>
2024-04-16 19:27:36 +0100

    Refactor Account page to fix userId type and update DashboardInvestors component

--- src/app/profile/investments/account/page.tsx
+++ src/app/profile/investments/account/page.tsx
@@ -5,7 +5,7 @@ import { UserProps } from '@/types'
 import Link from 'next/link'
 import Balance from './_Balance'
 
-export default async function Account({ userId }: { userId: string }) {
+export default async function Account({ userId }: { userId?: string }) {
   const { userId: currentUserId } = await getAuth()
   const { shms_user_total_balance, shms_user_withdrawable_balance, shms_fullname } =
     (await getUser(userId ?? currentUserId)) as UserProps
